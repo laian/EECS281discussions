2d2bd9783511e5a9cb2b28536eebcef9ffa98dcc
Ian Lai <laian41@gmail.com>
2013-09-11 14:52:38 -0400

    here

--- stringstream.cpp
+++ stringstream.cpp
@@ -5,10 +5,19 @@
 using namespace std;
 
 int main(){
+	string name;
+	cout << "I can say your name ten times really fast.  What's your name?"
+	cin << name;
+
 	stringstream ss;
 	for(int i = 0; i < 10; i++){
-		ss << "count " << i << '\n';
+		ss << name << ' ' << i << '\n'; // newlines or \n is faster than endl
 	}
+
 	cout << ss.str();
+	cout.flush(); //prints out buffer without adding newline
+
+	cerr << "I'm done!\n"; //the error stream does not get redirected
+
 	return 0;
 }
