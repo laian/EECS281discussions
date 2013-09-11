290a1b71985fbb206eae3c1a116096fff17daf3f
Ian Lai <laian41@gmail.com>
2013-09-11 13:07:21 -0400

    adding leaks

--- example.cpp
+++ example.cpp
@@ -3,7 +3,17 @@
 using namespace std;
 
 int main(){
-	int x = rand()%100;
-	cout << "Haha, you don't know what x is!\n";
-	int y = 100; //breakpoint goes here
+	int x = 100; //start watching x
+	cout << "x is " << x << " but I'm going to change it!\n";
+	x = rand()%100; // x is a random integer between 0 and 99;
+	cout << "Haha, you don't know what x is!\n"; 
+	x = rand()%100;
+	cout << "Also, I have memory leaks\n";
+	leak();
+	return 0;
+}
+
+void leak(){
+	x = new int();
+	return;
 }
